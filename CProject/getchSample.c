09fa207f44346763014b0f7f756f1a9c39857dc3
영준 조 <5907022@stu.kmu.ac.kr>
2024-03-12 09:49:27 +0900

    소수의 합

--- CProject/getchSample.c
+++ CProject/getchSample.c
@@ -1,94 +1,9 @@
-//#include <stdio.h>
-//
-//int main(void)
-//{
-//	char szName[32] = { 0 };
-//
-//	printf("당신의 이름을 입력하세요");
-//	gets(szName);
-//
-//
-//	printf("당신의 이름은");
-//	
-//	puts(szName);
-//	puts("입니다.");
-//
-//}
-
-
-//int main(void)
-//{
-//	char szName[32] = { 0 };
-//
-//	printf("이름을 입력하세요");
-//	gets(szName);
-//
-//	printf("당신의 이름은");
-//	puts(szName);
-//	puts("입니다");
-//
-//	return 0;
-//}
-
-//#include <stdio.h>
-//
-//int main(void)
-//{
-//	char sBuffer[8] = { 0 };
-//
-//	gets_s(sBuffer, sizeof(sBuffer));
-//	puts(sBuffer);
-//}
-
-
-
-//int main(void)
-//{
-//	char ch = 0;
-//	scanf_s("%c", & ch, 1);
-//	printf("입력한 문자는 %c 입니다",ch);
-//
-//	return 0;
-//
-//
-//}
-
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-//int main(void)
-//{
-//	char szBuffer[4] = { 0 };
-//	scanf_s("%4c", &szBuffer, _countof(szBuffer));
-//	printf("%c%c%c%c\n", szBuffer[0], szBuffer[1], szBuffer[2], szBuffer[3]);
-//
-//	return 0;
-//}
-
-
-//#include <stdio.h>
-//
-//int main(void)
-//{
-//	char ch[32] = { 0 };
-//	int nAge = 0;
-//	printf("이름을 입력하세요");
-//	gets_s(ch , sizeof(ch));
-//
-//	printf("나이를 입력하세요");
-//	scanf_s("%d", &nAge);
-//
-//	printf("%d세 %s\n", nAge, ch);
-//
-//	return 0;
-//}
-
-
 #include <stdio.h>
 
 int main(void)
 {
-	printf("오늘 수업은 여기까지");
+	printf("소수의 합")
+	
+	
 	return 0;
 }
-
