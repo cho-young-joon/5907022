a4b7c17bde3464f790f9f15155d633afdec5ac88
영준 조 <5907022@stu.kmu.ac.kr>
2024-03-12 09:51:48 +0900

    수정 test

--- CProject/getchSample.c
+++ CProject/getchSample.c
@@ -1,9 +1,104 @@
-#include <stdio.h>
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	char szName[32] = { 0 };
+//
+//	printf("당신의 이름을 입력하세요");
+//	gets(szName);
+//
+//
+//	printf("당신의 이름은");
+//	
+//	puts(szName);
+//	puts("입니다.");
+//
+//}
 
-int main(void)
+
+//int main(void)
+//{
+//	char szName[32] = { 0 };
+//
+//	printf("이름을 입력하세요");
+//	gets(szName);
+//
+//	printf("당신의 이름은");
+//	puts(szName);
+//	puts("입니다");
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	char sBuffer[8] = { 0 };
+//
+//	gets_s(sBuffer, sizeof(sBuffer));
+//	puts(sBuffer);
+//}
+
+
+
+//int main(void)
+//{
+//	char ch = 0;
+//	scanf_s("%c", & ch, 1);
+//	printf("입력한 문자는 %c 입니다",ch);
+//
+//	return 0;
+//
+//
+//}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(void)
+//{
+//	char szBuffer[4] = { 0 };
+//	scanf_s("%4c", &szBuffer, _countof(szBuffer));
+//	printf("%c%c%c%c\n", szBuffer[0], szBuffer[1], szBuffer[2], szBuffer[3]);
+//
+//	return 0;
+//}
+
+
+//#INCLUDE <STDIO.H>
+//
+//INT MAIN(VOID)
+//{
+//	CHAR CH[32] = { 0 };
+//	INT NAGE = 0;
+//	PRINTF("이름을 입력하세요");
+//	GETS_S(CH , SIZEOF(CH));
+//
+//	PRINTF("나이를 입력하세요");
+//	SCANF_S("%D", &NAGE);
+//
+//	PRINTF("%D세 %S\N", NAGE, CH);
+//
+//	RETURN 0;
+//}
+
+
+#define MAX_ELEMENTS 100
+int scores[MAX_ELEMENTS];
+
+int getMax_score(int n)
 {
-	printf("소수의 합")
-	
-	
-	return 0;
+
+	int largest;
+	largest = scores[0];
+
+	for (int i = 1; i < n; i++) {
+		if (scores[i] > largest) {
+			largest = scores[i];
+		}
+	}
+	return largest;
 }
+
+
